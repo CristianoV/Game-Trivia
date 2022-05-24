950db0cccd0fcbe2eebfa326241841a01a66cbd2
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-05-24 15:26:13 -0300

    controle de estado da pagina de login

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,15 +1,40 @@
 import React from 'react';
 
 class Login extends React.Component {
+  state = {
+    name: '',
+    email: '',
+  };
+
+  handleChange = ({ target: { id, value } }) => {
+    this.setState({
+      [id]: value,
+    });
+  };
+
   render() {
+    const { name, email } = this.state;
+
     return (
       <form>
         <label htmlFor="name">
-          <input type="text" data-testid="input-player-name" />
+          <input
+            type="text"
+            data-testid="input-player-name"
+            id="name"
+            value={ name }
+            onChange={ this.handleChange }
+          />
         </label>
 
         <label htmlFor="email">
-          <input type="email" data-testid="input-gravatar-email" />
+          <input
+            type="email"
+            data-testid="input-gravatar-email"
+            id="email"
+            value={ email }
+            onChange={ this.handleChange }
+          />
         </label>
 
         <button type="button" data-testid="btn-play">
