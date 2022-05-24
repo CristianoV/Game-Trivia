739239b9feccff346aa3c0fef21d0722346aca16
Jeazi Ricardo <jeazi_ricardo@hotmail.com>
2022-05-24 19:27:01 -0300

    Testa o redecionamentos dos botões Play e Settings

--- src/tests/Login.test.js
+++ src/tests/Login.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
+import App from '../App';
 import Login from '../pages/Login';
 import { screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -29,5 +30,26 @@ describe('Testes do componente Login', () => {
     expect(name.value).toBe('Jeazi');
     expect(email.value).toBe('jeazi_ricardo@hotmail.com');
     expect(btnPlay.disabled).toBe(false);
+  });
+
+  test('03 - Testa se ao clicar no botão Play a página é redirecionada', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const email = screen.getByLabelText(/e-mail/i);
+    const name = screen.getByLabelText(/name/i);
+    const btnPlay = screen.getByRole('button', { name: /play/i });
+    userEvent.type(name, 'Jeazi');
+    userEvent.type(email, 'jeazi_ricardo@hotmail.com');
+    userEvent.click(btnPlay);
+    const { pathname } = history.location;
+    console.log(history);
+    expect(pathname).toBe('/game');
+  });
+
+  test('04 - Testa se ao clicar no botão Settings a página é redirecionada', () => {
+    const { history } = renderWithRouterAndRedux(<App />);
+    const btnSet = screen.getByRole('button', { name: /settings/i });
+    userEvent.click(btnSet);
+    const { pathname } = history.location;
+    expect(pathname).toBe('/settings');
   });
 });
