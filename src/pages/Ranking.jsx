524dca606b83384753c5a94636a1f124e4f184d9
Vitor Soares <pereiravitor1218@gmail.com>
2022-05-25 15:41:53 -0300

    cria função que ao cicar retorna para o login

--- src/pages/Ranking.jsx
+++ src/pages/Ranking.jsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 class Ranking extends React.Component {
+  backToLogin = () => {
+    const { history } = this.props;
+    history.push('/');
+  }
+
   render() {
     return (
       <main>
+        <button
+          type="button"
+          data-testid="btn-go-home"
+          onClick={ this.backToLogin }
+        >
+          Login
+        </button>
       </main>
     );
   }
 }
+
+Ranking.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Ranking;
