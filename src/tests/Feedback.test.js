3f62ef5722cc771ac8366b5a41ce0164dbc93379
Cristiano Vieira <cristianoviieira@gmail.com>
2022-05-26 01:26:59 -0300

    requisito 17 finalizado, testes da page de Feedback prontos

--- src/tests/Feedback.test.js
+++ src/tests/Feedback.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
 import renderWithRouterAndRedux from './helpers/renderWithRouterAndRedux';
 import App from '../App';
-// import Feedback from '../pages/Feedback';
 import { screen, waitFor } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 
 describe('Testes da pagina de Feedback', () => {
     test('01 - Testa se os elementos estão na tela', () => {    
@@ -26,5 +25,64 @@ describe('Testes da pagina de Feedback', () => {
       })
       expect(srcImage).toBeInTheDocument();
       expect(srcImage.src).toBe(`https://www.gravatar.com/avatar/${initialState.player.hash}`)
+    });
+
+
+    test('02 - Testa se a frase do score esta correta', () => {    
+      const initialState = {
+        player: {
+          name: 'Player Name',
+          gravatarEmail: 'player@email.com',
+          score: 3,
+          assertions: 0,
+          hash: '123123',
+        }
+      }     
+      renderWithRouterAndRedux(<App />, initialState, "/feedback")
+      const scorePlayerFrase = screen.queryByText(/Could be better/i)
+      expect(scorePlayerFrase).not.toBeInTheDocument();
+      
+    });
+
+
+    test('03 - testando o redirecionamento do botão Play Again', () => {    
+      const initialState = {
+        player: {
+          name: 'Player Name',
+          gravatarEmail: 'player@email.com',
+          score: 3,
+          assertions: 0,
+          hash: '123123',
+        }
+      }     
+      const {history} = renderWithRouterAndRedux(<App />, initialState, "/feedback")
+      const playAgainButton = screen.getByRole('button', {
+        name: /play again/i
+      })
+      expect(playAgainButton).toBeInTheDocument();
+      userEvent.click(playAgainButton);
+      const { pathname } = history.location;
+      expect(pathname).toBe('/game');
+   
+    });
+
+    
+    test('04 - testando o redirecionamento do botão Rankig', () => {    
+      const initialState = {
+        player: {
+          name: 'Player Name',
+          gravatarEmail: 'player@email.com',
+          score: 3,
+          assertions: 0,
+          hash: '123123',
+        }
+      }     
+      const {history} = renderWithRouterAndRedux(<App />, initialState, "/feedback")
+      const buttonRanking = screen.getByRole('button', {
+        name: /ranking/i
+      })
+      expect(buttonRanking).toBeInTheDocument();
+      const { pathname } = history.location;
+      expect(pathname).toBe('/ranking');
     });
 });
