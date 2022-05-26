05e83a32496a553d7661551549de379f19625952
BrunoNowk <brunopoliselli2010@hotmail.com>
2022-05-26 17:28:09 -0300

    para o timer ao clicar no botão

--- src/pages/Game.jsx
+++ src/pages/Game.jsx
@@ -10,7 +10,7 @@ class Game extends React.Component {
     shuffle: [],
     isRunning: true,
     time: 30,
-  }
+  };
 
   componentDidMount() {
     this.getQuestions();
@@ -24,21 +24,25 @@ class Game extends React.Component {
   }
 
   tokenDenied = () => {
-    const { history: { push } } = this.props;
+    const {
+      history: { push },
+    } = this.props;
     localStorage.removeItem('token');
     push('/');
-  }
+  };
 
   getQuestions = async () => {
     const denied = 3;
     const token = localStorage.getItem('token');
-    const request = await fetch(`https://opentdb.com/api.php?amount=5&token=${token}`);
+    const request = await fetch(
+      `https://opentdb.com/api.php?amount=5&token=${token}`,
+    );
     const json = await request.json();
     if (json.response_code === denied) return this.tokenDenied();
     this.setState({ questions: json.results }, () => {
       this.shuffleQuestions();
     });
-  }
+  };
 
   shuffleArray = (array) => {
     const arr = [...array];
@@ -47,18 +51,23 @@ class Game extends React.Component {
       [arr[i], arr[j]] = [arr[j], arr[i]];
     }
     return arr;
-  }
+  };
 
   shuffleQuestions = () => {
     const { questions, questionIndex } = this.state;
-    const array = [questions[questionIndex].correct_answer,
-      ...questions[questionIndex].incorrect_answers];
+    const array = [
+      questions[questionIndex].correct_answer,
+      ...questions[questionIndex].incorrect_answers,
+    ];
     const shuffle = this.shuffleArray(array);
     const correct = shuffle.indexOf(array[0]);
     this.setState({ shuffle, correct });
-  }
+  };
 
-  setAnswer = () => this.setState({ isRunning: false });
+  setAnswer = ({ target }) => {
+    clearInterval(this.interval);
+    this.setState({ isRunning: false }, () => console.log(target.className));
+  };
 
   verifyAnswer = (answer, correct) => (answer === correct ? 'correct' : 'incorrect');
 
@@ -84,8 +93,8 @@ class Game extends React.Component {
     return (
       <div>
         <header>
-          <p data-testid="header-player-name">{ name }</p>
-          <p data-testid="header-score">{ score }</p>
+          <p data-testid="header-player-name">{name}</p>
+          <p data-testid="header-score">{score}</p>
           <img
             src={ `https://www.gravatar.com/avatar/${hash}` }
             alt="profileimage"
@@ -93,47 +102,40 @@ class Game extends React.Component {
           />
         </header>
         <main>
-          <span>{ time }</span>
-          {
-            questions.length && (
-              <div>
-                <p
-                  data-testid="question-category"
-                >
-                  { questions[questionIndex].category }
-                </p>
-                <p>{ questions[questionIndex].difficulty }</p>
-                <p
-                  data-testid="question-text"
-                >
-                  { questions[questionIndex].question }
-                </p>
-                <div data-testid="answer-options">
-                  {
-                    shuffle.map((question, i) => (
-                      <button
-                        disabled={ time <= 0 }
-                        onClick={ this.setAnswer }
-                        key={ i }
-                        value={ question }
-                        type="button"
-                        className={
-                          !isRunning && this.verifyAnswer(question, shuffle[correct])
-                        }
-                        data-testid={
-                          question === shuffle[correct]
-                            ? 'correct-answer'
-                            : `wrong-answer-${i}`
-                        }
-                      >
-                        { question }
-                      </button>
-                    ))
-                  }
-                </div>
+          <span>{time}</span>
+          {questions.length && (
+            <div>
+              <p data-testid="question-category">
+                {questions[questionIndex].category}
+              </p>
+              <p>{questions[questionIndex].difficulty}</p>
+              <p data-testid="question-text">
+                {questions[questionIndex].question}
+              </p>
+              <div data-testid="answer-options">
+                {shuffle.map((question, i) => (
+                  <button
+                    disabled={ time <= 0 || !isRunning }
+                    onClick={ (e) => this.setAnswer(e) }
+                    key={ i }
+                    value={ question }
+                    type="button"
+                    className={
+                      !isRunning
+                      && this.verifyAnswer(question, shuffle[correct])
+                    }
+                    data-testid={
+                      question === shuffle[correct]
+                        ? 'correct-answer'
+                        : `wrong-answer-${i}`
+                    }
+                  >
+                    {question}
+                  </button>
+                ))}
               </div>
-            )
-          }
+            </div>
+          )}
         </main>
       </div>
     );
